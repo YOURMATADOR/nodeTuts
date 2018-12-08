9629b101d0ebf577c9e439152c8941aa239a6625
Eduardo Avila (TUMATADOR) <jesus.amancilla@alumnos.udg.mx>
2018-12-08 15:29:26 -0600

    creacion, lectura de archivos y carpetas :alien:

---
+++ callbacks.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+const path = require('path')
+const direcctorio = path.join(__dirname, 'temp')
+const contenido = __filename
+const nuevoArchivo = path.join(direcctorio, 'nuevoArchivo.js')
+
+const miCallback = (err, res) => {
+	if (err) {
+		console.log('Error al leer el archivo')
+	} else {
+		console.log('El contenido del archivo es \n', res)
+	}
+}
+
+fs.readFile('archivo.json', 'utf8', miCallback)
+
+// * crear un directorio con un nuevo archivo con el mismo contenido de este archivo
+
+// const tratarError = (err) => console.log(err)
+
+const hacerDirecctorio = (nombre) => fs.mkdirSync(nombre)
+
+const leerArchivo = (nombre) => fs.readFileSync(nombre, 'utf8')
+
+const escribirArchivo = (nombre, txt) => fs.writeFileSync(nombre, txt)
+
+// const crearNuevoArchivo = (carpeta,texto,nombre) =>
+// hacerDirecctorio(carpeta)
+// .then((result) => {
+//     leerArchivo(texto)
+// })
+// .then(()=>
+// escribirArchivo(nombre))
+// .then(()=> console.log('Archivo creado correctamente'))
+// .catch((err) => {
+//     console.log('Error al escribir el nuevo archivo');
+// });
+// const crearNuevoArchivo = async (carpeta, texto, nombre) => {
+// 	try 
+// 	{
+// 		var m = await hacerDirecctorio(carpeta)
+// 		var x = await leerArchivo(texto)
+// 		console.log(x)
+// 		var l = await escribirArchivo(nombre, x)
+// 	} catch (error) {
+// 		console.log(error)
+// 	}
+// }
+// crearNuevoArchivo(direcctorio, contenido, nuevoArchivo)
+
+
+
+const revizarPorError = (cb) => {
+    return (err,res) => {
+        if(err)
+        tratarError(err)
+        else
+        cb(res)
+    }
+}
+
+const tratarError = (err) => console.log(err);
